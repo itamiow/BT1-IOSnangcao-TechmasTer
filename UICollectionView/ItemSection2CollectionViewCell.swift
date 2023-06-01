82c433475042a559d78bc4c1269d49178ca6e462
quangvien <quangvien92@gmail.com>
2023-06-01 09:18:39 +0700

    update

---
+++ UICollectionView/ItemSection2CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemSection2CollectionViewCell.swift
+//  UICollectionView
+//
+//  Created by USER on 31/05/2023.
+//
+
+import UIKit
+
+class ItemSection2CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
